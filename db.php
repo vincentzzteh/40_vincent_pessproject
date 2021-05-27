50133ca195cc4dba74d9a3f4f3cd45bb95e8479e
vincentzzteh <83630643+vincentzzteh@users.noreply.github.com>
2021-05-27 13:47:46 +0800

    Initial commit

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php // define database connection variables as constant
+define('DB_USER', "username"); // db user
+define('DB_PASSWORD', "password"); // db password 
+define('DB_DATABASE', "pessdb"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
